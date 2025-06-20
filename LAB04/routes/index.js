5af2c5f4cea492fc8cd64827d8e3459c407c5f24
SUKHWINDERKAU <sukhwinderkaur769610@gmail.com>
2025-06-20 09:13:11 -0400

    coomit

---
+++ LAB04/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Welcome' });
+});
+
+router.get('/taylor', function(req, res) {
+  res.render('taylor');
+});
+
+router.get('/drake', function(req, res) {
+  res.render('drake');
+});
+
+router.get('/billie', function(req, res) {
+  res.render('billie');
+});
+
+router.get('/weeknd', function(req, res) {
+  res.render('weeknd');
+});
+
+module.exports = router;
